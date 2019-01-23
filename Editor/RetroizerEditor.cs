de36eedecd539b15af61dca1f3991b6448a746f3
Thomas ICHÉ <thomasi@unity3d.com>
2019-01-23 15:07:40 +0100

    Fixes for Package

--- Editor/RetroizerEditor.cs
+++ Editor/RetroizerEditor.cs
@@ -1,52 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Retroizer))]
-public class RetroizerEditor : Editor {
-
-    protected Dictionary<string, SerializedProperty> m_Properties;
-    
-    private void OnEnable()
+namespace Retroizer
+{
+    [CustomEditor(typeof(Retroizer))]
+    public class RetroizerEditor : Editor
     {
-        if (m_Properties == null)
-            m_Properties = new Dictionary<string, SerializedProperty>();
+        protected Dictionary<string, SerializedProperty> m_Properties;
 
-        AddProperty("m_TargetCamera");
-        AddProperty("m_Resolution");
-        AddProperty("m_Orientation");
-        AddProperty("m_CustomWidth");
-        AddProperty("m_CustomHeight");
-
-    }
-
-    private void AddProperty(string name)
-    {
-        var prop = serializedObject.FindProperty(name);
-        if(prop != null)
-            m_Properties.Add(name, prop);
-    }
-
-    private void OnDisable()
-    {
-        m_Properties.Clear();
-    }
-
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-        EditorGUI.BeginChangeCheck();
-        EditorGUILayout.PropertyField(m_Properties["m_Resolution"], false);
-        if(m_Properties["m_Resolution"].intValue == (int)Retroizer.Resolution._Custom)
+        private void OnEnable()
         {
-            EditorGUILayout.PropertyField(m_Properties["m_CustomWidth"], false);
-            EditorGUILayout.PropertyField(m_Properties["m_CustomHeight"], false);
-        }
-        EditorGUILayout.PropertyField(m_Properties["m_Orientation"], false);
-        EditorGUILayout.PropertyField(m_Properties["m_TargetCamera"], false);
+            if (m_Properties == null)
+                m_Properties = new Dictionary<string, SerializedProperty>();
 
-        if (EditorGUI.EndChangeCheck())
-            serializedObject.ApplyModifiedProperties();
+            AddProperty("m_TargetCamera");
+            AddProperty("m_Resolution");
+            AddProperty("m_Orientation");
+            AddProperty("m_CustomWidth");
+            AddProperty("m_CustomHeight");
+
+        }
+
+        private void AddProperty(string name)
+        {
+            var prop = serializedObject.FindProperty(name);
+            if (prop != null)
+                m_Properties.Add(name, prop);
+        }
+
+        private void OnDisable()
+        {
+            m_Properties.Clear();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(m_Properties["m_Resolution"], false);
+            if (m_Properties["m_Resolution"].intValue == (int)Retroizer.Resolution._Custom)
+            {
+                EditorGUILayout.PropertyField(m_Properties["m_CustomWidth"], false);
+                EditorGUILayout.PropertyField(m_Properties["m_CustomHeight"], false);
+            }
+            EditorGUILayout.PropertyField(m_Properties["m_Orientation"], false);
+            EditorGUILayout.PropertyField(m_Properties["m_TargetCamera"], false);
+
+            if (EditorGUI.EndChangeCheck())
+                serializedObject.ApplyModifiedProperties();
+        }
     }
+
 }
